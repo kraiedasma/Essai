416f7dcabf3f767160a0b2447656b510d3f61317
mariem ammar <mariem.ammar1992@hotmail.com>
2017-01-26 03:39:50 -0800

    classe personne

---
+++ App1/App1/Model/Personne.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.Model
+{
+    class Personne
+    {
+        public String nom { get; set; }
+
+        public String prenom { get; set; }
+
+        public int cin{ get; set; }
+
+        public String email { get; set; }
+
+    }
+}
